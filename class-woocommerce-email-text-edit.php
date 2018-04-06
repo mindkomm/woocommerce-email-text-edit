7fd7b56380becd4e58d46b56f75baf51b0e63320
Lukas Gaechter <lukas.gaechter@mind.ch>
2018-04-06 16:00:21 +0200

    Improve filters

--- class-woocommerce-email-text-edit.php
+++ class-woocommerce-email-text-edit.php
@@ -4,7 +4,7 @@
  */
 class WooCommerce_Email_Text_Edit {
 	/**
-	 * Emails where an additional form field is added.
+	 * Emails where an additional form field is added before the order details.
 	 *
 	 * Other available types not included in this list:
 	 *
@@ -12,7 +12,7 @@ class WooCommerce_Email_Text_Edit {
 	 *
 	 * @var array Array of email IDs.
 	 */
-	public $customer_email_types = array(
+	public $editable_emails = array(
 		// Default WooCommerce E-Mails
 		'customer_completed_order',
 		'customer_invoice',
@@ -30,11 +30,11 @@ class WooCommerce_Email_Text_Edit {
 	);
 
 	/**
-	 * Emails where the already existing header content will be removed.
+	 * Emails where the already existing content before the order details will be removed.
 	 *
 	 * @var array Array of email IDs.
 	 */
-	public $customer_email_types_filtered = array(
+	public $filtered_emails = array(
 		'customer_completed_order',
 		'customer_invoice',
 		'customer_processing_order',
@@ -48,11 +48,37 @@ class WooCommerce_Email_Text_Edit {
 	);
 
 	/**
-	 * WooCommerce_Email_Text_Edit constructor.
+	 * Email_Text_Edit_For_WooCommerce constructor.
 	 */
 	public function init() {
+		/**
+		 * Filters email IDs that should have an edit field in the settings.
+		 *
+		 * For all emails passed in this list, a textarea will be added to the settings where the
+		 * email text can be edited.
+		 *
+		 * @api
+		 * @since 1.0.0
+		 *
+		 * @param array $editable_emails An array of email IDs that should be editable.
+		 */
+		$this->editable_emails = apply_filters( 'wc_ete/emails/editable', $this->editable_emails );
+
+		/**
+		 * Filters email IDs where default content should be removed.
+		 *
+		 * For all emails passed in this list the default content that is defined in the email
+		 * template before the order details will be removed.
+		 *
+		 * @api
+		 * @since 1.0.0
+		 *
+		 * @param array $filtered_emails An array of email IDs where the default content should be removed.
+		 */
+		$this->filtered_emails = apply_filters( 'wc_ete/emails/filtered', $this->filtered_emails );
+
 		// Add form fields for different email types
-		foreach ( $this->customer_email_types as $type ) {
+		foreach ( $this->editable_emails as $type ) {
 			add_filter( "woocommerce_settings_api_form_fields_{$type}", [
 				$this,
 				'add_form_fields',
@@ -85,7 +111,7 @@ class WooCommerce_Email_Text_Edit {
 	}
 
 	/**
-	 * Start catching content.
+	 * Start catching content with an output buffer.
 	 *
 	 * @param string   $email_heading Email heading.
 	 * @param WC_Email $email         Email object.
@@ -102,7 +128,7 @@ class WooCommerce_Email_Text_Edit {
 	}
 
 	/**
-	 * Release content into ether.
+	 * Release content buffer into ether.
 	 *
 	 * @param WC_Order $order         Order object.
 	 * @param bool     $sent_to_admin Whether email is sent to admin.
@@ -126,13 +152,13 @@ class WooCommerce_Email_Text_Edit {
 	 * @return bool
 	 */
 	public function email_should_be_filtered( $email ) {
-		return in_array( $email->id, $this->customer_email_types_filtered, true );
+		return in_array( $email->id, $this->filtered_emails, true );
 	}
 
 	/**
 	 * Checks whether email text is is empty.
 	 *
-	 * @param WC_Email $email
+	 * @param WC_Email $email The email to check.
 	 *
 	 * @return bool
 	 */
@@ -160,8 +186,30 @@ class WooCommerce_Email_Text_Edit {
 			'{firstname}' => $this->get_first_name( $order ),
 		);
 
+		/**
+		 * Filters placeholder contents.
+		 *
+		 * You can use this to add your own placeholders.
+		 *
+		 * @api
+		 * @since 1.0.0
+		 * @example
+		 * ```php
+		 * add_filter( 'wc_ete_placeholders_content', function( $placeholders ) {
+		 *     return $placeholders;
+		 * } );
+		 * ```
+		 *
+		 * @param array    $placeholders  A key-value array of placeholders where the key is the
+		 *                                placeholder and the value is the content that should be
+		 *                                used instead of the placeholder in the email text.
+		 * @param WC_Email $email         A WooCommerce email object.
+		 * @param WC_Order $order         A WooCommerce order object.
+		 * @param bool     $sent_to_admin Whether the email will be sent to an admin.
+		 * @param bool     $plain_text    Whether the email is sent in plaintext.
+		 */
 		$placeholders = apply_filters(
-			'wc_ete_placeholders_content',
+			'wc_ete/placeholders/content',
 			$placeholders,
 			$email,
 			$order,
@@ -213,7 +261,8 @@ class WooCommerce_Email_Text_Edit {
 		$fields['email_text_before_content'] = array(
 			'title'       => __( 'Email text before content', 'woocommerce-email-text-edit' ),
 			'type'        => 'textarea',
-			'description' => __( 'Available Placeholders', 'woocommerce-email-text-edit' ) . $this->create_placeholder_descriptions(),
+			'description' => __( 'Available Placeholders', 'woocommerce-email-text-edit' )
+								. $this->create_placeholder_descriptions(),
 			'default'     => '',
 		);
 
@@ -233,12 +282,28 @@ class WooCommerce_Email_Text_Edit {
 				'Add only this tag to the content to leave email content empty',
 				'woocommerce-email-text-edit'
 			),
-			'{first_name}' => __( 'First name from billing address', 'woocommerce-email-text-edit' ),
+			'{first_name}' => __(
+				'First name from billing address',
+				'woocommerce-email-text-edit'
+			),
 		);
 
-		$descriptions = apply_filters( 'wc_ete_placeholders_description', $descriptions );
+		/**
+		 * Filter placeholder description used in admin area.
+		 *
+		 * @api
+		 * @since 1.0.0
+		 *
+		 * @param array $descriptions A key-value array of descriptions where the key is the
+		 *                            placeholder and the key is the description of that placeholder.
+		 */
+		$descriptions = apply_filters( 'wc_ete/placeholders/description', $descriptions );
 
 		$html = '';
+
+		if ( empty( $descriptions ) ) {
+			return $html;
+		}
 
 		foreach ( $descriptions as $tag => $description ) {
 			$html .= '<br><code>' . $tag . '</code>: ' . $description;
