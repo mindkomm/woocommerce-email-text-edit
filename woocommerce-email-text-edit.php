919828d57f5261b62d970a2ad0ac90937ba06f8c
Lukas Gaechter <lukas.gaechter@mind.ch>
2019-05-08 17:41:46 +0200

    Make it work as a theme package

--- woocommerce-email-text-edit.php
+++ woocommerce-email-text-edit.php
@@ -14,12 +14,16 @@ if ( ! class_exists( 'WooCommerce_Email_Text_Edit' ) ) {
 	require_once 'lib/class-woocommerce-email-text-edit.php';
 }
 
-add_action( 'plugins_loaded', function() {
+add_action( 'init', function() {
 	if ( ! class_exists( 'WooCommerce' ) ) {
 		return;
 	}
 
-	load_plugin_textdomain( 'woocommerce-email-text-edit', false, basename( dirname( __FILE__ ) ) . '/languages/' );
+	$locale  = get_locale();
+	$path    = trailingslashit( dirname( __FILE__ ) );
+	$mo_path = $path . "languages/woocommerce-email-text-edit-{$locale}.mo";
+
+	load_textdomain( 'woocommerce-email-text-edit', $mo_path );
 
 	if ( class_exists( 'WooCommerce_Email_Text_Edit' ) ) {
 		$wc_ete = new WooCommerce_Email_Text_Edit();
